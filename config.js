c645a37cd8fb93bdf0efb8e1ab71ac9893aacc8b
alfreadrorw1 <alfreadrorw@gmail.com>
2026-02-05 21:04:03 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+{
+  "owner": "60136951175",
+  "user": [
+    "628xxxxxxxxxxx",
+    "628xxxxxxxxxxx"
+  ],
+  "pairingText": "ZYPHERON",
+  "botName": "Alf Oxylus",
+  "sticker": {
+    "packname": "Created by",
+    "author": "Zypheron"
+  }
+}
